e8470381a28dba1ae547fb313649cbcc184fac43
dhea <dhea.angelina@ti.ukdw.ac.id>
2024-04-13 17:40:21 +0700

    [Add] Listview Sort

--- ProjekAkhir_MediaPlayer/Form1.vb
+++ ProjekAkhir_MediaPlayer/Form1.vb
@@ -404,6 +404,64 @@ Class formLagu
         AxWindowsMediaPlayer1.Ctlcontrols.currentPosition = barLagu.Value
     End Sub
 
+    Private Sub lstLagu_ColumnClick(sender As Object, e As ColumnClickEventArgs) Handles lstLagu.ColumnClick
+        For Each column As ColumnHeader In lstLagu.Columns
+            column.Text = column.Text.Split(" ")(0)
+        Next
+
+        If lstLagu.Sorting = SortOrder.Ascending Then
+            lstLagu.Sorting = SortOrder.Descending
+            If lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text & " ▲" Then
+                lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text.Replace(" ▲", "") & " ▼"
+            Else
+                lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text.Replace(" ▲", "").Replace(" ▼", "")
+            End If
+            lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text & " ▼"
+        Else
+            lstLagu.Sorting = SortOrder.Ascending
+            If lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text & " ▼" Then
+                lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text.Replace(" ▼", "") & " ▲"
+            Else
+                lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text.Replace(" ▲", "").Replace(" ▼", "")
+            End If
+            lstLagu.Columns(e.Column).Text = lstLagu.Columns(e.Column).Text & " ▲"
+        End If
+
+        lstLagu.ListViewItemSorter = New ListViewItemComparer(e.Column, lstLagu.Sorting)
+        lstLagu.Sort()
+    End Sub
+
+    Private Class ListViewItemComparer
+        Implements IComparer
+
+        Private col As Integer
+        Private order As SortOrder
+
+        Public Sub New(column As Integer, order As SortOrder)
+            col = column
+            Me.order = order
+        End Sub
+
+        Public Function Compare(x As Object, y As Object) As Integer Implements IComparer.Compare
+            Dim returnVal As Integer = -1
+
+            If IsNumeric(CType(x, ListViewItem).SubItems(col).Text) AndAlso IsNumeric(CType(y, ListViewItem).SubItems(col).Text) Then
+                'Numeric
+                returnVal = Val(CType(x, ListViewItem).SubItems(col).Text).CompareTo(Val(CType(y, ListViewItem).SubItems(col).Text))
+            Else
+                'String
+                returnVal = [String].Compare(CType(x, ListViewItem).SubItems(col).Text, CType(y, ListViewItem).SubItems(col).Text)
+            End If
+
+            'Descending.
+            If order = SortOrder.Descending Then
+                returnVal *= -1
+            End If
+
+            Return returnVal
+        End Function
+    End Class
+
     Private Sub BarVolume_Scroll(sender As Object, e As EventArgs) Handles BarVolume.Scroll
         AxWindowsMediaPlayer1.settings.volume = BarVolume.Value
         If BarVolume.Value > 50 Then
@@ -414,6 +472,4 @@ Class formLagu
             picVolume.Image = My.Resources.no_volume
         End If
     End Sub
-
-
 End Class
